8294c0eeefd757c69a8846f3824e6c7d1d541771
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-03 17:03:10 -0400

    username search form complete

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     this.state = {
       user: {},
       followers: [],
+      input: '',
       username: 'amguenoun'
     }
   }
@@ -28,15 +29,45 @@ class App extends React.Component {
   }
 
 
-  handleUserChange = (login) => {
-    this.setState({ username: login });
+  handleUserSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ username: this.state.input }, () => {
+      axios.get(`https://api.github.com/users/${this.state.username}`)
+        .then(res => this.setState({ user: res.data }))
+        .catch(err => console.log('App: CMD: Axios: ', err))
+
+      axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+        .then(res => this.setState({ followers: res.data }))
+        .catch(err => console.log('App: CDU: Axios: ', err))
+    });
+  }
+
+  // componentDidUpdate() {
+  //   if (this.state.username === this.state.input) {
+  //     axios.get(`https://api.github.com/users/${this.state.username}`)
+  //       .then(res => this.setState({ user: res.data }))
+  //       .catch(err => console.log('App: CMD: Axios: ', err))
+
+  //     axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+  //       .then(res => this.setState({ followers: res.data }))
+  //       .catch(err => console.log('App: CDU: Axios: ', err))
+  //   }
+  // }
+
+  handleUserChange = (e) => {
+    this.setState({ input: e.target.value });
   }
 
   render() {
-    console.log(this.state.username)
+    console.log(this.state.input, this.state.username);
     return (
       <div className="App">
         <h1>GitHub User Card</h1>
+        <form onSubmit={this.handleUserSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input name='username' type="text" placeholder='Github Username' value={this.state.input} onChange={this.handleUserChange} />
+          <button type='submit'>Submit</button>
+        </form>
         <UserCard user={this.state.user} key={this.state.user.id} />
         <h2>Followers: </h2>
         {this.state.followers.map(follower => <FollowerCard key={follower.id} username={follower.login} />)}
