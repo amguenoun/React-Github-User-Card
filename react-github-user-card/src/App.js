367299abf622b91f18fe55ca9ee4dd8d8e8ec283
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-03 15:40:54 -0400

    app changed to class and axios request made in CMD

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Bye</p>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      username: 'amguenoun'
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.username}`)
+      .then(res => console.log(res.data))
+      .catch(err => console.log('App: CMD: Axios: ', err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Yo</p>
+      </div>
+    );
+  }
 }
 
 export default App;
