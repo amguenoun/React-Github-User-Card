c325ca6281da50bb550f36c63c781a74ddbde0e6
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-03 16:24:33 -0400

    created follower card component and displayed followers

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import UserCard from './components/UserCard';
+import FollowerCard from './components/FollowerCard'
 
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
     super();
     this.state = {
       user: {},
+      followers: [],
       username: 'amguenoun'
     }
   }
@@ -21,11 +23,19 @@ class App extends React.Component {
       .catch(err => console.log('App: CMD: Axios: ', err))
   }
 
+  componentDidUpdate() {
+    axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+      .then(res => this.setState({ followers: res.data }))
+      .catch(err => console.log('App: CDU: Axios: ', err))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>GitHub User Card</h1>
-        <UserCard user={this.state.user} />
+        <UserCard user={this.state.user} key={this.state.user.id} />
+        <h2>Followers: </h2>
+        {this.state.followers.map(follower => <FollowerCard key={follower.id} username={follower.login} />)}
       </div>
     );
   }
