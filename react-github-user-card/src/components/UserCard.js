38607e404efcfa25cb0f4ab505ce052ad8a79208
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-03 18:46:12 -0400

    added profile link to github page

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -9,6 +9,7 @@ const UserCard = (props) => {
             <div className="text-div">
                 <h2>{props.user.name}</h2>
                 <h4>{props.user.login}</h4>
+                <p>Profile: <a target='blank' href={props.user.html_url}>{props.user.html_url}</a></p>
                 <p>Location: {props.user.location === null ? "N/A" : props.user.location}</p>
                 <p>Bio: {props.user.bio === null ? "N/A" : props.user.bio}</p>
                 <p>Followers: {props.user.followers}</p>
